c69cc3b3c9e47bb2504c031e7b11219d89272885
Tamas Alexandru Ilie <atamas@signal.co>
2019-09-12 16:40:33 +0300

    new feature PM

--- ConnectionsPool.php
+++ ConnectionsPool.php
@@ -3,9 +3,11 @@
 use Colors\Color;
 use React\Socket\ConnectionInterface;
 
-Class ConnectionsPool {
+class ConnectionsPool
+{
 
-    Protected $connections;
+    protected $connections;
+    protected $pmConstant = '#pm';
 
     public function __construct()
     {
@@ -19,8 +21,8 @@ Class ConnectionsPool {
      *
      * @return void
      */
-    public function add(ConnectionInterface $connection) {
-
+    public function add(ConnectionInterface $connection)
+    {
         $connection->write((new Color("Welcome to the channel!\n"))->fg("green"));
         $connection->write("Enter your username:");
         $this->setConnectionName($connection, '');
@@ -34,21 +36,51 @@ Class ConnectionsPool {
      *
      * @return void
      */
-    private function handelEvents(ConnectionInterface $connection){
-        $connection->on('data', function($data) use ($connection) {
+    private function handelEvents(ConnectionInterface $connection)
+    {
+        $connection->on('data', function ($data) use ($connection) {
             $name = $this->getConnectionName($connection);
-            if(empty($name)){
+
+            if (empty($name)) {
                 $this->addNewMember($connection, $data);
                 return;
             }
-            $this->sendAll((new Color("$name:"))->bold() ." $data", $connection);
+
+            if (substr($data, 0, 3) == $this->pmConstant) {
+                $targetName = $this->getNameFromString($data);
+                $data = str_replace(($this->pmConstant . ' ' . $targetName . ' '), '', $data);
+                $tagetConnection = $this->getObjectbyName($targetName);
+                if ($tagetConnection) {
+                    $tagetConnection->write((new Color("PM|$name:"))->bold()->fg('magenta') . " $data");
+                } else {
+                    $connection->write((new Color("The user " . $targetName . " is offline!\n"))->fg('magenta'));
+                }
+            } else {
+                $this->sendAll((new Color("$name:"))->bold() . " $data", $connection);
+            }
         });
 
-        $connection->on('close', function() use ($connection) {
+        $connection->on('close', function () use ($connection) {
             $name = $this->getConnectionName($connection);
             $this->connections->offsetUnset($connection);
-            $this->sendAll((new Color("The user ".$name." just left the cahnnel!\n"))->fg("red"), $connection);
+            $this->sendAll((new Color("The user " . $name . " just left the cahnnel!\n"))->fg("red"), $connection);
         });
+    }
+
+    /**
+     * getNameFromString
+     *
+     * @param  String $data
+     *
+     * @return mixt
+     */
+    private function getNameFromString($data)
+    {
+        preg_match("/" . $this->pmConstant . "\s([^\s]+)\s/", $data, $name);
+
+        if (isset($name[1])) {
+            return trim($name[1]);
+        }
     }
 
     /**
@@ -57,11 +89,12 @@ Class ConnectionsPool {
      * @param  String $message
      * @param  ConnectionInterface $except
      *
-     * @return void
+     * @return null
      */
-    private function sendAll($message, ConnectionInterface $except){
-        foreach($this->connections as $connection){
-            if($connection !== $except){
+    private function sendAll($message, ConnectionInterface $except)
+    {
+        foreach ($this->connections as $connection) {
+            if ($connection !== $except) {
                 $connection->write($message);
             }
         }
@@ -74,7 +107,8 @@ Class ConnectionsPool {
      *
      * @return void
      */
-    private function getConnectionName(ConnectionInterface $connection){
+    private function getConnectionName(ConnectionInterface $connection)
+    {
         return $this->connections->offsetGet($connection);
     }
 
@@ -86,7 +120,8 @@ Class ConnectionsPool {
      *
      * @return void
      */
-    private function setConnectionName(ConnectionInterface $connection, $name){
+    private function setConnectionName(ConnectionInterface $connection, $name)
+    {
         $this->connections->offsetSet($connection, $name);
     }
 
@@ -98,9 +133,34 @@ Class ConnectionsPool {
      *
      * @return void
      */
-    private function addNewMember(ConnectionInterface $connection, $name){
+    private function addNewMember(ConnectionInterface $connection, $name)
+    {
         $name = str_replace(["\n", "\r"], '', $name);
         $this->setConnectionName($connection, $name);
         $this->sendAll((new Color("User $name joined the channel\n"))->fg("blue"), $connection);
+    }
+
+    /**
+     * getObjectbyName
+     *
+     * @param  String $name
+     *
+     * @return mixt
+     */
+    private function getObjectbyName($name)
+    {
+        $this->connections->rewind();
+        while ($this->connections->valid()) {
+            $object = $this->connections->current();
+            $data = $this->connections->getInfo();
+            if ($name === $data) {
+                $this->connections->rewind();
+
+                return $object;
+            }
+            $this->connections->next();
+        }
+
+        return null;
     }
 }
